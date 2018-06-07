a515c41ccf01ed750030308676f2841fee72f222
angelazaragueta <36796940+angelazaragueta@users.noreply.github.com>
2018-06-07 17:10:05 +0200

    Add files via upload

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,11 +1,29 @@
 const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 const {models} = require("../models");
+const cloudinary = require('cloudinary');
+const fs = require('fs');
+const attHelper = require("../helpers/attachments");
 
+const paginate = require('../helpers/paginate').paginate;
+
+// Optios for the files uploaded to Cloudinary
+const cloudinary_upload_options = {
+    async: true,
+    folder: "/core/quiz2018/attachments",
+    resource_type: "auto",
+    tags: ['core', 'quiz']
+};
 
 // Autoload the quiz with id equals to :quizId
 exports.load = (req, res, next, quizId) => {
 
-    models.quiz.findById(quizId)
+    models.quiz.findById(quizId, {
+        include: [
+            {model: models.tip,include:[{model: models.user, as: 'author'}]},
+            {model: models.user, as: 'author'}
+        ]
+    })
     .then(quiz => {
         if (quiz) {
             req.quiz = quiz;
@@ -18,12 +36,142 @@ exports.load = (req, res, next, quizId) => {
 };
 
 
+
+// MW that allows actions only if the user logged in is admin or is the author of the quiz.
+exports.adminOrAuthorRequired = (req, res, next) => {
+
+    const isAdmin  = !!req.session.user.isAdmin;
+    const isAuthor = req.quiz.authorId === req.session.user.id;
+
+    if (isAdmin || isAuthor) {
+        next();
+    } else {
+        console.log('Prohibited operation: The logged in user is not the author of the quiz, nor an administrator.');
+        res.send(403);
+    }
+};
+
+
 // GET /quizzes
 exports.index = (req, res, next) => {
 
-    models.quiz.findAll()
+    let countOptions = {
+        where: {},
+        include: []
+    };
+
+    const searchfavourites = req.query.searchfavourites || "";
+
+    let title = "Questions";
+
+    // Search:
+    const search = req.query.search || '';
+    if (search) {
+        const search_like = "%" + search.replace(/ +/g,"%") + "%";
+
+        countOptions.where.question = { [Op.like]: search_like };
+    }
+
+    // If there exists "req.user", then only the quizzes of that user are shown
+    if (req.user) {
+        countOptions.where.authorId = req.user.id;
+
+        if (req.session.user && req.session.user.id == req.user.id) {
+            title = "My Questions";
+        } else {
+            title = "Questions of " + req.user.username;
+        }
+    }
+
+    // Filter: my favourite quizzes:
+    if (req.session.user) {
+        if (searchfavourites) {
+            countOptions.include.push({
+                model: models.user,
+                as: "fans",
+                where: {id: req.session.user.id},
+                attributes: ['id']
+
+            });
+        } else {
+
+            // NOTE:
+            // It should be added the options ( or similars )
+            // to have a lighter query:
+            //    where: {id: req.session.user.id},
+            //    required: false  // OUTER JOIN
+            // but this does not work with SQLite. The generated
+            // query fails when there are several fans of the same quiz.
+
+            countOptions.include.push({
+                model: models.user,
+                as: "fans",
+                attributes: ['id']
+            });
+        }
+    }
+
+    models.quiz.count(countOptions)
+    .then(count => {
+
+        // Pagination:
+
+        const items_per_page = 10;
+
+        // The page to show is given in the query
+        const pageno = parseInt(req.query.pageno) || 1;
+
+        // Create a String with the HTMl used to render the pagination buttons.
+        // This String is added to a local variable of res, which is used into the application layout file.
+        res.locals.paginate_control = paginate(count, items_per_page, pageno, req.url);
+
+        const findOptions = {
+            ...countOptions,
+            offset: items_per_page * (pageno - 1),
+            limit: items_per_page
+        };
+
+        findOptions.include.push(models.attachment);
+        findOptions.include.push({
+            model: models.user,
+            as: 'author'
+        });
+
+        return models.quiz.findAll(findOptions);
+    })
     .then(quizzes => {
-        res.render('quizzes/index.ejs', {quizzes});
+
+        const format = (req.params.format || 'html').toLowerCase();
+
+        switch (format) {
+            case 'html':
+
+                // Mark favourite quizzes:
+                if (req.session.user) {
+                    quizzes.forEach(quiz => {
+                        quiz.favourite = quiz.fans.some(fan => {
+                            return fan.id == req.session.user.id;
+                        });
+                    });
+                }
+
+                res.render('quizzes/index.ejs', {
+                    quizzes,
+                    search,
+                    searchfavourites,
+                    cloudinary,
+                    title
+                });
+                break;
+
+            case 'json':
+                res.json(quizzes);
+                break;
+
+            default:
+                console.log('No supported format \".'+format+'\".');
+                res.sendStatus(406);
+        }
     })
     .catch(error => next(error));
 };
@@ -34,7 +182,47 @@ exports.show = (req, res, next) => {
 
     const {quiz} = req;
 
-    res.render('quizzes/show', {quiz});
+    const format = (req.params.format || 'html').toLowerCase();
+
+    switch (format) {
+        case 'html':
+
+            new Promise((resolve, reject) => {
+
+                // Only for logger users:
+                //   if this quiz is one of my fovourites, then I create
+                //   the attribute "favourite = true"
+                if (req.session.user) {
+                    resolve(
+                        req.quiz.getFans({where: {id: req.session.user.id}})
+                        .then(fans => {
+                            if (fans.length > 0) {
+                                req.quiz.favourite = true;
+                            }
+                        })
+                    );
+                } else {
+                    resolve();
+                }
+            })
+            .then(() => {
+                res.render('quizzes/show', {
+                    quiz,
+                    cloudinary
+                });
+            })
+            .catch(error => next(error));
+
+            break;
+
+        case 'json':
+            res.json(quiz);
+            break;
+
+        default:
+            console.log('No supported format \".'+format+'\".');
+            res.sendStatus(406);
+    }
 };
 
 
@@ -42,7 +230,7 @@ exports.show = (req, res, next) => {
 exports.new = (req, res, next) => {
 
     const quiz = {
-        question: "", 
+        question: "",
         answer: ""
     };
 
@@ -54,23 +242,64 @@ exports.create = (req, res, next) => {
 
     const {question, answer} = req.body;
 
+    const authorId = req.session.user && req.session.user.id || 0;
+
     const quiz = models.quiz.build({
         question,
-        answer
+        answer,
+        authorId
     });
 
     // Saves only the fields question and answer into the DDBB
-    quiz.save({fields: ["question", "answer"]})
+    quiz.save({fields: ["question", "answer", "authorId"]})
     .then(quiz => {
         req.flash('success', 'Quiz created successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        if (!req.file) {
+            req.flash('info', 'Quiz without attachment.');
+            res.redirect('/quizzes/' + quiz.id);
+            return;
+        }
+
+        // Save the attachment into  Cloudinary
+        return attHelper.checksCloudinaryEnv()
+        .then(() => {
+            return attHelper.uploadResourceToCloudinary(req.file.path, cloudinary_upload_options);
+        })
+        .then(uploadResult => {
+
+            // Create the new attachment into the data base.
+            return models.attachment.create({
+                public_id: uploadResult.public_id,
+                url: uploadResult.url,
+                filename: req.file.originalname,
+                mime: req.file.mimetype,
+                quizId: quiz.id })
+            .then(attachment => {
+                req.flash('success', 'Image saved successfully.');
+            })
+            .catch(error => { // Ignoring validation errors
+                req.flash('error', 'Failed to save file: ' + error.message);
+                cloudinary.api.delete_resources(uploadResult.public_id);
+            });
+
+        })
+        .catch(error => {
+            req.flash('error', 'Failed to save attachment: ' + error.message);
+        })
+        .then(() => {
+            fs.unlink(req.file.path); // delete the file uploaded at./uploads
+            res.redirect('/quizzes/' + quiz.id);
+        });
     })
     .catch(Sequelize.ValidationError, error => {
+
         req.flash('error', 'There are errors in the form:');
         error.errors.forEach(({message}) => req.flash('error', message));
         res.render('quizzes/new', {quiz});
     })
     .catch(error => {
+
         req.flash('error', 'Error creating a new Quiz: ' + error.message);
         next(error);
     });
@@ -97,7 +326,64 @@ exports.update = (req, res, next) => {
     quiz.save({fields: ["question", "answer"]})
     .then(quiz => {
         req.flash('success', 'Quiz edited successfully.');
-        res.redirect('/quizzes/' + quiz.id);
+
+        if (!body.keepAttachment) {
+
+            // There is no attachment: Delete old attachment.
+            if (!req.file) {
+                req.flash('info', 'This quiz has no attachment.');
+                if (quiz.attachment) {
+                    cloudinary.api.delete_resources(quiz.attachment.public_id);
+                    quiz.attachment.destroy();
+                }
+                return;
+            }
+
+            // Save the new attachment into Cloudinary:
+            return attHelper.checksCloudinaryEnv()
+            .then(() => {
+                return attHelper.uploadResourceToCloudinary(req.file.path, cloudinary_upload_options);
+            })
+            .then(function (uploadResult) {
+
+                // Remenber the public_id of the old image.
+                const old_public_id = quiz.attachment ? quiz.attachment.public_id : null;
+
+                // Update the attachment into the data base.
+                return quiz.getAttachment()
+                .then(function(attachment) {
+                    if (!attachment) {
+                        attachment = models.attachment.build({ quizId: quiz.id });
+                    }
+                    attachment.public_id = uploadResult.public_id;
+                    attachment.url = uploadResult.url;
+                    attachment.filename = req.file.originalname;
+                    attachment.mime = req.file.mimetype;
+                    return attachment.save();
+                })
+                .then(function(attachment) {
+                    req.flash('success', 'Image saved successfully.');
+                    if (old_public_id) {
+                        cloudinary.api.delete_resources(old_public_id);
+                    }
+                })
+                .catch(function(error) { // Ignoring image validation errors
+                    req.flash('error', 'Failed saving new image: '+error.message);
+                    cloudinary.api.delete_resources(uploadResult.public_id);
+                });
+
+
+            })
+            .catch(function(error) {
+                req.flash('error', 'Failed saving the new attachment: ' + error.message);
+            })
+            .then(function () {
+                fs.unlink(req.file.path); // delete the file uploaded at./uploads
+            });
+        }
+    })
+    .then(function () {
+        res.redirect('/quizzes/' + req.quiz.id);
     })
     .catch(Sequelize.ValidationError, error => {
         req.flash('error', 'There are errors in the form:');
@@ -114,10 +400,18 @@ exports.update = (req, res, next) => {
 // DELETE /quizzes/:quizId
 exports.destroy = (req, res, next) => {
 
+    // Delete the attachment at Cloudinary (result is ignored)
+    if (req.quiz.attachment) {
+        attHelper.checksCloudinaryEnv()
+        .then(() => {
+            cloudinary.api.delete_resources(req.quiz.attachment.public_id);
+        });
+    }
+
     req.quiz.destroy()
     .then(() => {
         req.flash('success', 'Quiz deleted successfully.');
-        res.redirect('/quizzes');
+        res.redirect('/goback');
     })
     .catch(error => {
         req.flash('error', 'Error deleting the Quiz: ' + error.message);
@@ -133,10 +427,32 @@ exports.play = (req, res, next) => {
 
     const answer = query.answer || '';
 
-    res.render('quizzes/play', {
-        quiz,
-        answer
-    });
+    new Promise(function (resolve, reject) {
+
+        // Only for logger users:
+        //   if this quiz is one of my fovourites, then I create
+        //   the attribute "favourite = true"
+        if (req.session.user) {
+            resolve(
+                req.quiz.getFans({where: {id: req.session.user.id}})
+                .then(fans => {
+                    if (fans.length > 0) {
+                        req.quiz.favourite = true
+                    }
+                })
+            );
+        } else {
+            resolve();
+        }
+    })
+    .then(() => {
+        res.render('quizzes/play', {
+            quiz,
+            answer,
+            cloudinary
+        });
+    })
+    .catch(error => next(error));
 };
 
 
@@ -156,91 +472,79 @@ exports.check = (req, res, next) => {
 };
 
 
-//Practica6 randomplay al azar una pregunta en BBDD,sin repetir la pregunta.
-//GET quizzes/randomplay
+
+// GET /quizzes/randomplay
 exports.randomplay = (req, res, next) => {
 
-
-
-    //Si session es vacio,desde 0
-    if(req.session.randomplay === undefined) {
-        req.session.nota = 0;
-
-        models.quiz.findAll()
-            .then(quiz => {
-                req.session.randomplay = quiz;
-                req.session.idofquiz = Math.floor(Math.random()*req.session.randomplay.length);
-                res.render('quizzes/random_play', {
-                    quiz : req.session.randomplay[req.session.idofquiz],
-                    score: req.session.nota
-                });
-
-            })
+    if(req.session.resolved === undefined){
+        req.session.resolved = [];
     }
-    //Ya exite alguna quiz en session.
-    else {
 
-        req.session.idofquiz = Math.floor(Math.random()*req.session.randomplay.length);
-        res.render('quizzes/random_play', {
-            quiz: req.session.randomplay[req.session.idofquiz],
-            score: req.session.nota
+    Sequelize.Promise.resolve().then(() => {
+
+        const whereOpt = {'id': {[Sequelize.Op.notIn]:req.session.resolved}};
+
+        return models.quiz.count({where: whereOpt})
+        .then( count => {
+            let score = req.session.resolved.length;
+            if(count === 0){
+                delete req.session.resolved;
+                res.render('quizzes/random_nomore', {score});
+            }
+
+            let rand = Math.floor(Math.random()*count);
+            return models.quiz.findAll({
+                where: whereOpt,
+                offset: rand,
+                limit: 1
+            })
+
+            .then(quizzes => {
+                return quizzes[0];
+            });
+        })
+        .catch(error => {
+            req.flash('error', 'Error deleting Quiz: ' + error.message);
+            next(error);
         });
 
-    }
-
-
-
+    }).then(quiz => {
+        console.log("QUIZ" + quiz);
+        let score = req.session.resolved.length;
+        res.render('quizzes/random_play', {quiz, score});
+    });
 
 };
+
 
 exports.randomcheck = (req, res, next) => {
-    let result = false;
-    let nota = -1;
 
 
+    req.session.resolved = req.session.resolved || [];
 
-    if(trimm(req.query.answer) === trimm(req.session.randomplay[req.session.idofquiz]).answer){
-        req.session.nota++;
-        nota = req.session.nota;
-        result = true;
-        req.session.randomplay.splice(req.session.idofquiz, 1);
+    const answer = req.query.answer || '';
+    const result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+    let score = req.session.resolved.length;
 
-
-        if(req.session.randomplay.length === 0) {
-
-                    res.render('quizzes/random_nomore', {
-                        score: req.session.nota
-                    });
-
-
+    if(result){
+        if(req.session.resolved.indexOf(req.quiz.id) === -1){
+            req.session.resolved.push(req.quiz.id);
+            score = req.session.resolved.length;
         }
-        else{
-                res.render('quizzes/random_result', {
-                    score: nota,
-                    result: result,
-                    answer: req.session.randomplay[req.session.idofquiz].answer
-                });
-        }
-    }
-    else{
-        nota = req.session.nota;
-        res.session.nota = 0;
 
-        res.render('quizzes/random_result', {
-            score: nota,
-            result: result,
-            answer: req.query.answer
+        models.quiz.count()
+        .then( count => {
+            if(score > count){
+                delete req.session.resolved;
+                res.render('quizzes/random_result', {result, score, answer});
+            }else{
+                res.render('quizzes/random_result', {result, score, answer});
+               
+            }
         });
+    }else{
+        let score = req.session.resolved.length;
+        delete req.session.resolved;
+        res.render('quizzes/random_result', {result, score, answer});
     }
-
-
-
-};
-
-trimm = rl => {
-    rl = rl.replace(/\s+/g,"");
-    rl = rl.toUpperCase();
-    rl = rl.toLowerCase();
-    rl = rl.trim();
-    return rl;
 };
